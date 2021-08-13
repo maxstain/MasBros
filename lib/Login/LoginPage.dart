350a8e3e7cea3a741124af68451508abb64a9485
maxstain <firaschabchoub@hotmail.com>
2021-08-13 12:00:31 +0100

    refactor: adding the auth system

---
+++ lib/Login/LoginPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:masbros/Login/components/Body.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
