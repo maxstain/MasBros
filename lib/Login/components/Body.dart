ff2edefe33197c14ff95375f0723a05e6da522c8
maxstain <firaschabchoub@hotmail.com>
2021-08-13 12:28:55 +0100

    refactor: adding the auth system

--- lib/Login/components/Body.dart
+++ lib/Login/components/Body.dart
@@ -13,7 +13,41 @@ class _BodyState extends State<Body> {
     return Container(
       child: ListView(
         children: [
-          Text("Login"),
+          Center(
+            heightFactor: 4.0,
+            child: Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 20.0),
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: "Email",
+              ),
+            ),
+          ),
+          Container(
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: "Password",
+              ),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.pink),
+            ),
+            child: Text("Login"),
+            onPressed: () {},
+          ),
         ],
       ),
     );
