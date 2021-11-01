9ebd2f5d53d32e1340b14eb4a1423a9dc607f75e
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-11-01 01:37:00 +0100

    refactor: working on the chat

--- lib/ChatPage/ChatPage.dart
+++ lib/ChatPage/ChatPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:masbros/ChatPage/components/Body.dart';
 
 class ChatPage extends StatefulWidget {
@@ -9,13 +10,70 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final messages = [
+    {
+      "msg": "This is a sent message ",
+      "time": "00:03",
+      "sender": "Ryzerrector",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a sent message ",
+      "time": "00:03",
+      "sender": "Ryzerrector",
+    },
+  ];
+  final TextEditingController chatController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Chat"),
       ),
-      body: Body(),
+      body: Body(msgs: messages),
+      persistentFooterButtons: [
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: "Type here....",
+          ),
+          textInputAction: TextInputAction.send,
+          controller: chatController,
+          onFieldSubmitted: (value) {
+            setState(() {
+              messages.add({
+                "msg": chatController.value.text,
+                "time": TimeOfDay.now().format(context),
+                "sender": "Ryzerrector",
+              });
+              print(messages);
+              chatController.text = "";
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.send,
+            color: Colors.pink,
+          ),
+          onPressed: () {
+            print("Sent");
+          },
+        ),
+      ],
     );
   }
 }
--- lib/ChatPage/components/Body.dart
+++ lib/ChatPage/components/Body.dart
@@ -1,17 +1,114 @@
 import 'package:flutter/material.dart';
 
 class Body extends StatefulWidget {
-  Body({Key? key}) : super(key: key);
+  List? msgs = [];
+  Body({Key? key, List? this.msgs}) : super(key: key);
 
   @override
   _BodyState createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
+  final messages = [
+    {
+      "msg": "This is a sent message ",
+      "time": "00:03",
+      "sender": "Ryzerrector",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a recieved message",
+      "time": "00:04",
+      "sender": "Sender",
+    },
+    {
+      "msg": "This is a sent message ",
+      "time": "00:03",
+      "sender": "Ryzerrector",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView(
+      child: ListView.builder(
+        itemCount: messages.length,
+        itemBuilder: (BuildContext context, i) {
+          if (messages[i]["sender"] == "Ryzerrector") {
+            return Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 3.0,
+                vertical: 10.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.pink,
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(messages[i]["msg"].toString()),
+                        Text("Sent on ${messages[i]["time"].toString()}"),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Icon(
+                      Icons.account_circle,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 3.0,
+                vertical: 10.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Icon(
+                      Icons.account_circle,
+                      color: Colors.pink,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.pink[200],
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(messages[i]["msg"].toString()),
+                        Text("Sent on ${messages[i]["time"].toString()}"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
+      /* ListView(
         children: [
           Container(
             child: Row(
@@ -20,6 +117,8 @@ class _BodyState extends State<Body> {
                   padding: EdgeInsets.symmetric(vertical: 10.0),
                   width: MediaQuery.of(context).size.width / 1.1,
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text("Text"),
                       Text("Sent on 23:11"),
@@ -48,6 +147,7 @@ class _BodyState extends State<Body> {
                 Container(
                   width: MediaQuery.of(context).size.width / 1.1,
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Text("Text"),
                       Text("Sent on 23:11"),
@@ -58,7 +158,7 @@ class _BodyState extends State<Body> {
             ),
           ),
         ],
-      ),
+      ), */
     );
   }
 }
